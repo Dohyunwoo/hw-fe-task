45e9c1552eab363429815c19f8d12281db2b18df
Hyunwoo Do <hwdo@cyglobal.net>
2024-06-12 22:15:03 +0900

    메인 화면(/register) 추가

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="uk-flex uk-flex-column uk-flex-center uk-flex-middle">
+    <div class="logo-area uk-margin-bottom">
+      <img src="https://i.balaan.io/mobile/img/icons/logo-blue.png" alt="logo">
+    </div>
+    <div class="form-area">
+      <component :is="componentList[step]"></component> 
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import PersonalInfo from "@/components/PersonalInfo.vue";
+  import AddressInfo from "@/components/AddressInfo.vue";
+  import PaymentInfo from "@/components/PaymentInfo.vue";
+  import Complete from "@/components/Complete.vue";
+
+  import {step} from "@/store/user"
+
+  const componentList = [
+    PersonalInfo, AddressInfo, PaymentInfo, Complete
+  ]
+</script>
+
+<style scoped>
+  .form-area {
+    width: 100%;
+    border: 2px solid black;
+    padding: 40px 20px 20px 20px;
+  }
+</style>
